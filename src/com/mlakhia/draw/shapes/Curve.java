c90fe16d1b63d32cfd5fae51172ba252fe6e12a7
Michael Lakhia <mike@diputs.ca>
2013-04-11 20:59:48 -0400

    Start Curve Tool Implementation.

---
+++ src/com/mlakhia/draw/shapes/Curve.java
@@ -0,0 +1,39 @@
+package com.mlakhia.draw.shapes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+public class Curve extends Shape {
+
+	private List<Point> points = new ArrayList<Point>();
+
+	public Curve(List<Point> points, int strokeColor, int strokeWidth) {
+		this(points, strokeColor, strokeWidth, Color.TRANSPARENT);
+	}		
+	
+	public Curve( List<Point> points, int strokeColor, int strokeWidth, int fillColor) {
+		super(strokeColor, fillColor, strokeWidth);
+		this.points = points;
+	}
+
+	@Override
+	public void draw(Paint paint, Canvas canvas) {
+		// reset any path effect
+		paint.setPathEffect(null);
+							
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setColor(strokeColor);
+		paint.setStrokeWidth(strokeWidth);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		
+		for(int i = 0; i < points.size()-1; i++) {
+	        canvas.drawLine(points.get(i).x, points.get(i).y, points.get(i+1).x, points.get(i+1).y, paint);
+	    }		
+	}
+
+}
--- src/com/mlakhia/draw/tools/CurveTool.java
+++ src/com/mlakhia/draw/tools/CurveTool.java
@@ -1,52 +1,83 @@
 package com.mlakhia.draw.tools;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.mlakhia.draw.ToolBox;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 
 public class CurveTool extends Tool {
 
 	public CurveTool(ToolBox toolbox, ToolName name) {
 		super(toolbox, name);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void touchStart(MotionEvent event) {
-		// TODO Auto-generated method stub
+		Point point = new Point();
+		point.x = (int) event.getX();
+		point.y = (int) event.getY();
 		
-	}
-
-	@Override
-	public void touchEnd(MotionEvent event) {
-		// TODO Auto-generated method stub
 		
+        
+        hasPreview = false;
 	}
 
 	@Override
 	public void touchMove(MotionEvent event) {
-		// TODO Auto-generated method stub
+		Point point = new Point();
+		point.x = (int) event.getX();
+		point.y = (int) event.getY();
 		
+		
+		
+		hasPreview = true;
+		
+		toolbox.getDrawingView().invalidate();
 	}
-
+	
+	@Override
+	public void touchEnd(MotionEvent event) {
+		Point point = new Point();
+		point.x = (int) event.getX();
+		point.y = (int) event.getY();
+		
+		
+        
+		hasPreview = false;
+		
+		addToDrawing();
+		
+		toolbox.getDrawingView().invalidate();
+	}
+	
+	@Override
+	public void examplePreview(Canvas canvas) {
+		//com.mlakhia.draw.shapes.Path path = new com.mlakhia.draw.shapes.Path(getExamplePoints(), toolbox.getStrokeColor(), toolbox.getStrokeWidth(), toolbox.getFillColor());
+		//path.draw(new Paint(), canvas);
+	}
+	
 	@Override
 	public void drawPreview(Canvas canvas) {
-		
-
-		
+		if(toolbox.isExampleDotted()){
+			//for(int i = 0; i < points.size()-1; i++) {
+		    //    canvas.drawLine(points.get(i).x, points.get(i).y, points.get(i+1).x, points.get(i+1).y, toolbox.getPreviewPaint());
+		    //}
+		}else{
+			//com.mlakhia.draw.shapes.Path path = new com.mlakhia.draw.shapes.Path(points, toolbox.getStrokeColor(), toolbox.getStrokeWidth());
+			//path.draw(toolbox.getPreviewPaint(), canvas);
+		}
 	}
 
 	@Override
 	public void addToDrawing() {
-		// TODO Auto-generated method stub
-		
+		//com.mlakhia.draw.shapes.Path path = new com.mlakhia.draw.shapes.Path(points, toolbox.getStrokeColor(), toolbox.getStrokeWidth(), toolbox.getFillColor());
+		//toolbox.getDrawingView().getShapes().add(path);
 	}
-
-	@Override
-	public void examplePreview(Canvas canvas) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
